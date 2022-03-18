409d2ec5aa194370a292242c488fef38aa1d86c4
Conner Kulbaski <connerkulbaski@gmail.com>
2022-03-18 17:26:07 -0500

    Created app core

---
+++ BoredGames/App.js
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+function HomeScreen(){
+  return (
+    <View style={styles.container}>
+      <Text>Home screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function SearchScreen(){
+  return (
+    <View style={styles.container}>
+      <Text>Search</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function ProfileScreen(){
+  return (
+    <View style={styles.container}>
+      <Text>Profile</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+      <NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name = "Home" component={HomeScreen} />
+            <Tab.Screen name = "Search" component={SearchScreen} />
+            <Tab.Screen name = "Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 BoredGames
 a project for COMP 4020
 
+dependencies:
+node v12 or higher
+expo-cli
+``npm i -g expo-cli``
+
