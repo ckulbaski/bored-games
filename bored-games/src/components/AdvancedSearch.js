1d64436785d40e8dfa87043d5a7ea4f36e35771c
Conner Kulbaski <connerkulbaski@gmail.com>
2022-03-31 15:27:26 -0500

    advanced search implemented

--- bored-games/src/components/AdvancedSearch.js
+++ bored-games/src/components/AdvancedSearch.js
@@ -2,9 +2,22 @@ import * as React from 'react'
 let cat = [];
 
 
-function AdvancedSearch(){
+function AdvancedSearch(props){
 
     const [categories, setCategory] = React.useState("");
+
+    const onChangeCat = (event) =>{
+        props.setCategory(event.target.value);
+    }
+    const onChangeAge = (event) =>{
+        props.setAgeRange(event.target.value);
+    }
+    const onChangeNum = (event) =>{
+        props.setNumPlayers(event.target.value);
+    }
+    const onChangeComplex = (event) =>{
+        props.setComplexity(event.target.value);
+    }
 
     React.useEffect(async () => {
 
@@ -18,75 +31,42 @@ function AdvancedSearch(){
         //so I condensed it to this for now
         cat = require('../Assets/Categories.json');
 
-        console.log(cat.categories)
+        //console.log(cat.categories)
         setCategory(cat.categories)
     }, []);
 
     function getOption(){
     }
 
-    /*
-    function getOption() {
-        let catcat = document.getElementById('Category').value
-        console.log(games)
-        let newGames = games
-        console.log(newGames)
-        if (games !== "") {
-            console.log("catcat = ", catcat)
-
-            //  loop through every game
-            for (var key in newGames) {
-                let people = false
-                // similarly every ID in category and check
-                for (var keys in newGames[key].categories) {
-                    console.log(newGames[key].name)
-                    console.log(newGames[key].categories[keys].id)
-                    console.log(catcat)
-                    console.log(catcat === newGames[key].categories[keys].id)
-                    if (newGames[key].categories[keys].id == catcat) {
-                        people = true;
-                    }
-                }
-
-                if (people == false) {
-                    console.log("deleting ", games[key].name)
-                    delete newGames[key]
-                }
-
-            }
-        }
-        for (var key in newGames) {
-            console.log(newGames[key].name);
-        }
-        // does not re-render page for some reason
-        setGames(newGames)
-
-    }
-    */
     return(
         <div>
-        <select name="Category" id="Category" onChange={getOption}>
+        <div>
+        <select name="Category" id="Category" onChange={onChangeCat}>
             <option>Category</option>
             {categories !== "" ? categories.map(category => (
                 <option value={category.id} >{category.name}</option>
             ))
                 : null}
         </select>
+        </div>
+        <div>
         <input
             placeholder="Age range"
-            id="searchBar"
-            onChange={onChangeSearch}
+            onChange={onChangeAge}
         />
+        </div>
+        <div>
         <input
             placeholder="Number of players"
-            id="searchBar"
-            onChange={onChangeSearch}
+            onChange={onChangeNum}
         />
+        </div>
+        <div>
         <input
-            placeholder="Number of players"
-            id="searchBar"
-            onChange={onChangeSearch}
+            placeholder="Complexity"
+            onChange={onChangeComplex}
         />
+        </div>
         </div>
     );
 
--- bored-games/src/components/Search.js
+++ bored-games/src/components/Search.js
@@ -14,14 +14,32 @@ let cat = [];
 
 function Search() {
 
-    const [search, setSearch] = React.useState("");
+    const [name, setName] = React.useState("");
     const [games, setGames] = React.useState("");
-    const [categories, setCategory] = React.useState("");
+    const [category, setCategory] = React.useState("");
     const [ageRange, setAgeRange] = React.useState("");
     const [complexity,setComplexity] = React.useState("");
     const [numPlayers,setNumPlayers] = React.useState("");
-    function onChangeSearch(event) {
-        setSearch(event.target.value);
+
+    function onChangeName(event) {
+        console.log(event.target.value);
+        setName(event.target.value);
+    }
+    function onChangeCategory(value) {
+        console.log(value);
+        setCategory(value);
+    }
+    function onChangeNumPlayers(value) {
+        console.log(value);
+        setNumPlayers(value);
+    }
+    function onChangeAgeRange(value) {
+        console.log(value);
+        setAgeRange(value);
+    }
+    function onChangeComplexity(value) {
+        console.log(value);
+        setComplexity(value);
     }
 
     const toggleAdvancedSearchVisibility = () =>{
@@ -31,8 +49,17 @@ function Search() {
     }
 
     async function fetchSearch() {
-        let response = await fetch('https://api.boardgameatlas.com/api/search?name=' + search + client);
+        let n = name === "" ? "" : "&name="+name;
+        let age = age === undefined ? "" : "&age="+ageRange;
+        let num = num === undefined ? "" : "&max_players="+numPlayers;
+        let query = 'https://api.boardgameatlas.com/api/search?fuzzy_match=true' +n + age + num;
 
+        query += "&fields=name,description,image_url,average_user_rating,categories";
+        query += "&limit=100";
+        query+=client;
+        console.log(query);
+
+        let response = await fetch(query);
         console.log(response.status); // 200
         console.log(response.statusText); // OK
 
@@ -40,15 +67,62 @@ function Search() {
             json = await response.json();
             // handle data
         }
-        setGames(json.games);
-        console.log(json.games)
-        //console.log(json.games)
+        console.log(json.games);
+        var gameList=[];
         for (var key in json.games) {
-            console.log(json.games[key].name);
+            for(var cat in json.games[key].categories){
+                console.log(json.games[key].categories[cat].id);
+                if(json.games[key].categories[cat].id == category){
+                    gameList.push(json.games[key]);
+                    break;
+                }
+            }
         }
+        console.log(gameList);
+        setGames(gameList);
 
     }
 
+    
+    /*
+    function getOption() {
+        let catcat = document.getElementById('Category').value
+        console.log(games)
+        let newGames = games
+        console.log(newGames)
+        if (games !== "") {
+            console.log("catcat = ", catcat)
+
+            //  loop through every game
+            for (var key in newGames) {
+                let people = false
+                // similarly every ID in category and check
+                for (var keys in newGames[key].categories) {
+                    console.log(newGames[key].name)
+                    console.log(newGames[key].categories[keys].id)
+                    console.log(catcat)
+                    console.log(catcat === newGames[key].categories[keys].id)
+                    if (newGames[key].categories[keys].id == catcat) {
+                        people = true;
+                    }
+                }
+
+                if (people == false) {
+                    console.log("deleting ", games[key].name)
+                    delete newGames[key]
+                }
+
+            }
+        }
+        for (var key in newGames) {
+            console.log(newGames[key].name);
+        }
+        // does not re-render page for some reason
+        setGames(newGames)
+
+    }
+    */
+    
 
 
     return (
@@ -57,7 +131,7 @@ function Search() {
                 className='search-bar'
                 placeholder="Search"
                 id="searchBar"
-                onChange={onChangeSearch}
+                onChange={onChangeName}
             />
             <button type="button" onClick={fetchSearch}> Search </button>
             <button type="button" onClick={toggleAdvancedSearchVisibility}>
@@ -65,7 +139,7 @@ function Search() {
             </button>
             <div className="advanced-search"
                 id="AdvancedSearch">
-                <AdvancedSearch setCategory={setCategory} setAgeRange={setAgeRange} setNumPlayers={setNumPlayers} setComplexity={setComplexity}/>
+                <AdvancedSearch setCategory={onChangeCategory} setAgeRange={onChangeAgeRange} setNumPlayers={onChangeNumPlayers} setComplexity={onChangeComplexity}/>
             </div>
 
             {games !== "" ?
